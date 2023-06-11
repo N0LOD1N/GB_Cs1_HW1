70c62bd5f15fc3898c634a8b2e4e3406358419b3
Nik <hemper@mail.ru>
2023-06-11 16:47:13 +0300

    Problem 8 solved

---
+++ Problem8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int index = 2;
+
+while (index <= num)
+{
+    Console.Write($"{index}");
+    index++;
+    index++;
+    if (index <= num) Console.Write(", ");
+    else    Console.Write(".");
+}
